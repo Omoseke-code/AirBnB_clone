4201208683534912d047f89507cc3f6edb4cb150
Omoseke Salami <omosekesalami@gmail.com>
2023-03-11 13:50:14 -0800

    BaseModel including save, todict and init

---
+++ models/base_model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+import models
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    """The base model to the models in this package"""
+
+    def __init__(self, *args, **kwargs):
+        """Intialises the basemodel
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        timeform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, timeform)
+                else:
+                    self.__dict__[k] = v
+
+    def save(self):
+        """updates the public instance attribute updated_at
+        with the current datetime"""
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of
+        __dict__ of the instance"""
+        retdict = self.__dict__.copy()
+        retdict['created_at'] = self.created_at.isoformat()
+        retdict['updated_at'] = self.updated_at.isoformat()
+        retdict['__class__'] = self.__class__.__name__
+        return retdict
+
+    def __str__(self):
+        """Returns the string form of the class
+        Args:
+            id (str): id of the instance
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+"""Unittesting BaseModel
+Unittest cases:
+Unittest classes:
+    TestBaseModel_instantiation
+    TestBaseModel_save
+    TestBaseModel_to_dict
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.base_model import BaseModel
+
+
+class TestBaseModel_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the BaseModel class."""
+
+    def test_unique_id(self):
+        ob = BaseModel()
+        ob2 = BaseModel()
+        self.assertNotEqual(ob.id, ob2.id)
+
+    def test_updated_at(self):
+        ob = BaseModel()
+        sleep(0.05)
+        ob2 = BaseModel()
+        self.assertLess(ob.updated_at, ob2.updated_at)
+
+    def test_created_at(self):
+        bm1 = BaseModel()
+        sleep(0.05)
+        bm2 = BaseModel()
+        self.assertLess(bm1.created_at, bm2.created_at)
+
+    def test_str_representation(self):
+        dt = datetime.today()
+        dt_repr = repr(dt)
+        bm = BaseModel()
+        bm.id = "123456"
+        bm.created_at = bm.updated_at = dt
+        bmstr = bm.__str__()
+        self.assertIn("[BaseModel] (123456)", bmstr)
+        self.assertIn("'id': '123456'", bmstr)
+        self.assertIn("'created_at': " + dt_repr, bmstr)
+        self.assertIn("'updated_at': " + dt_repr, bmstr)
+
+    def test_instantiation_with_kwargs(self):
+        dt = datetime.today()
+        dt_iso = dt.isoformat()
+        bm = BaseModel(id="345", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(bm.id, "345")
+        self.assertEqual(bm.created_at, dt)
+        self.assertEqual(bm.updated_at, dt)
+
+class TestBaseModel_save(unittest.TestCase):
+    """Unittests for testing save method of the BaseModel class."""
+
+    def test_one_save(self):
+        bm = BaseModel()
+        sleep(0.05)
+        first_updated_at = bm.updated_at
+        bm.save()
+        self.assertLess(first_updated_at, bm.updated_at)
+
+    def test_two_saves(self):
+        bm = BaseModel()
+        sleep(0.05)
+        first_updated_at = bm.updated_at
+        bm.save()
+        second_updated_at = bm.updated_at
+        self.assertLess(first_updated_at, second_updated_at)
+        sleep(0.05)
+        bm.save()
+        self.assertLess(second_updated_at, bm.updated_at)
+
+class TestBaseModel_to_dict(unittest.TestCase):
+    """Unittests for testing to_dict method of the BaseModel class."""
+
+    def test_to_dict_contains_correct_keys(self):
+        bm = BaseModel()
+        self.assertIn("id", bm.to_dict())
+        self.assertIn("created_at", bm.to_dict())
+        self.assertIn("updated_at", bm.to_dict())
+        self.assertIn("__class__", bm.to_dict())
+
+    def test_to_dict_contains_added_attributes(self):
+        bm = BaseModel()
+        bm.name = "Halimah"
+        bm.my_number = 98
+        self.assertIn("name", bm.to_dict())
+        self.assertIn("my_number", bm.to_dict())
+
+    def test_to_dict_output(self):
+        dt = datetime.today()
+        bm = BaseModel()
+        bm.id = "123456"
+        bm.created_at = bm.updated_at = dt
+        tdict = {'id': '123456', '__class__': 'BaseModel', 'created_at': dt.isoformat(), 'updated_at': dt.isoformat()}
+        self.assertDictEqual(bm.to_dict(), tdict)
+                                                              
+if __name__ == "__main__":
+    unittest.main()
